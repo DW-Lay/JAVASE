db28e8277b33f11dbd30179b93b13f65a7e2c0b5
dongminglin <dongminglin1234@163.com>
2021-07-05 18:59:26 +0800

    修改了笔试代码

--- src/com/practice/practice2.java
+++ src/com/practice/practice2.java
@@ -14,50 +14,80 @@ import java.util.concurrent.ConcurrentHashMap;
  * @date 2021/4/30 18:49
  */
 public class practice2 {
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public static void main(String[] args) {
+//        System.out.println(new practice2().compressString("aabcccccaaa"));
+//        int[] p  =  {4,2,1};
+//        System.out.println(new practice2().petalsBreak(p));
+        int[][]rooms= {{-2,-3,3},{-5,-10,1},{10,30,-5}};
+        int[] start = {0,0};
+        int[] end  ={2,2};
+        System.out.println(new practice2().minimumInitHealth(rooms, start, end));
 
-//        String str1 = new String("test");
-//        String str2 = new String("test");
-//        String str3 = "test";
-//        String str4 = "test";
-//        System.out.println(str1==str2);
-//        System.out.println(str1.equals(str2));
-//        System.out.println(str1.hashCode()==str2.hashCode());
-//        System.out.println(str1.hashCode()==str3.hashCode());
-//        System.out.println(str3==str4);
-//        System.out.println(str3==str1);
-//        HashSet<String> strings = new HashSet<>();
-//        strings.add(str1);
-//        strings.add(str2);
-//        System.out.println(strings.size());
-//        strings.add(str3);
-//        System.out.println(strings.size());
-//        ConcurrentHashMap con = new ConcurrentHashMap();
+    }
 
-//        HashMap map = new HashMap<String,String>();
-//        map.put("name","zhang");
-//        map.put("name",null);
-//        map.put("","lis");
-//        System.out.println(map.size());
-//        System.out.println(map.get("name"));
-//
-//        Hashtable<Object, Object> table = new Hashtable<>();
-//          double i =2.1;
-//          long a =9223372036854775807L;
-//        List<Integer> list =  new ArrayList<Integer>();
-//        Method method = list.getClass().getMethod("add", Object.class); //要抛出个异常
-//        method.invoke(list,"测试");  // 也要抛出异常
-//        System.out.println(list.toString());
-        Integer int1 = 1;
-        Integer int2= new Integer(1);
-        Integer int3 =2-1;
-        Integer int4 = 200;
-        Integer int5 = 200;
-        int int6=200;
-        System.out.println(int1==int2);
-        System.out.println(int1==int3);
-        System.out.println(int4==int5);
-        System.out.println(int4==int6);
 
+    public int minimumInitHealth(int[][] rooms, int[] startPoint, int[] endPoint) {
+        // write code here
+        int res =0;
+        int m= rooms.length;
+        int n= rooms[0].length;
+        int[][] flag = new int[m][n];
+        res= dfs(startPoint[0],startPoint[1],endPoint[0],endPoint[1],flag,0,rooms);
+        return 0-res;
+    }
+    public int dfs(int x,int y,int x1,int y1,int[][] flag,int sum,int[][] rooms){
+        if(x>x1 ||x <0||y>y1 || y<0 || flag[x][y]==1){
+            return Integer.MIN_VALUE;
+        }
+        if(x==x1 && y ==y1){
+            return sum;
+        }
+        else {
+            flag[x][y]=1;
+            int res =  Math.max(dfs(x+1,y,x1,y1,flag,sum,rooms),dfs(x-1,y,x1,y1,flag,sum,rooms));
+            res =  Math.max(res,dfs(x,y+1,x1,y1,flag,sum,rooms));
+            res =  Math.max(res,dfs(x,y-1,x1,y1,flag,sum,rooms));
+            flag[x][y]=0;
+            return sum +=res;
+        }
+    }
+
+
+    public int petalsBreak(int[] petals) {
+        // write code here
+        int count=0;
+        for(int petal:petals ){
+            count +=(petal>>1)+(petal&1);
+        }
+        return count;
+    }
+
+    public String compressString(String param) {
+        // write code here
+        StringBuilder stringBuilder = new StringBuilder();
+        int strLength = param.length();
+        int count = 1;//记录重复个数
+        for (int i = 0; i < strLength-1; ++i) {
+            if (param.charAt(i) == param.charAt(i+1)) {
+                count++;
+            } else {
+                if (count == 1) {
+                    stringBuilder.append(param.charAt(i));
+                } else {
+                    stringBuilder.append(param.charAt(i));
+                    stringBuilder.append(count);
+
+                }
+                count = 1;//这句重置不要忘记
+            }
+        }
+        if (count == 1) {
+            stringBuilder.append(param.charAt(strLength-1));
+        } else {
+            stringBuilder.append(param.charAt(strLength-1));
+            stringBuilder.append(count);
+
+        }
+        return stringBuilder.toString();
     }
 }
