ccbaddc926571f6583ee87bca2ce20a380da0087
dongminglin <dongminglin1234@163.com>
2021-07-31 00:08:31 +0800

    更新了一些代码

---
+++ src/com/practice/abs.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+/**
+ * @program: HelloWorld
+ * @description: 测试抽象类
+ * @author: 董明林
+ * @date: 2021-07-25 17:15:04
+ */
+public abstract class abs {
+    public int ceshi(){
+        System.out.println("无参");
+        return 1;
+    }
+}
--- src/com/practice/practice2.java
+++ src/com/practice/practice2.java
@@ -1,5 +1,6 @@
 package com.practice;
 
+import javax.xml.bind.SchemaOutputResolver;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
@@ -34,9 +35,207 @@ public class practice2 {
 //        Map<Integer,Character> map   =  new HashMap<>();
 //        ConcurrentHashMap<Object, Object> objectObjectConcurrentHashMap = new ConcurrentHashMap<>();
 
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//        int[][] nums0 = new int[N][2];
+//        for(int i=0;i<N;i++){
+//            nums0[i][0] = sc.nextInt();
+//            nums0[i][1] = sc.nextInt();
+//        }
+//
+//        int[][] nums = {{2,3},{1,9},{4,10},{5,20}};
+//        int[][] nums1 = {{1,3},{4,5},{7,10},{13,18}};
+//        System.out.println(findbaohan(nums1));
+//        Scanner sc = new Scanner(System.in);
+//        int[] nums = new int[10];
+//        for(int i=0;i<10;i++ ){
+//            nums[i] = sc.nextInt();
+//        }
+
+//        int[] nums = {0,1,0,0,1,1,1,1,2,2};
+//        System.out.println(calculateMax(nums));
+
+//       int[] nums ={10,11,12,13,1};
+//       System.out.println(findDuiZi(nums));
+         int[][] books = {{20210427,20210504},{20210425,20210504},{20210425,20210504},{20210425,20210501},{20210425,20210501},{20210427,20210502}};
+         int[][] books2 ={{20210425,20210504}};
+        System.out.println(sort(books2));
+
+    }
+    public static int sort(int[][] books){
+
+        if(books.length<1){
+            return 0;
+        }
+        if(books.length==1){
+            return 1;
+        }
+        Arrays.sort(books, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] t1, int[] t2) {
+                if(t1[0]==t2[0]){
+                    return t1[1]-t2[1];
+                }else{
+                    return t1[0]-t2[0];
+                }
+//                return t1[0]-t2[0];
+
+            }
+        });
+        System.out.println(Arrays.deepToString(books));
+        int n =books.length;
+        int[] dp = new int[n];
+
+        dp[1]=1;
+        for(int i=1;i<n;i++){
+            if(books[i][1]>=books[i-1][1]){
+                dp[i] =dp[i-1]+1;
+            }else{
+                int j =i-1;
+                while(j>=0){
+                    if(books[i][1]>=books[j][1]){
+                        dp[i] = Math.max(dp[i-1],dp[j]+1);
+                        break;
+                    }
+                    if(books[i][0]>books[j][1]){
+                        dp[i] =dp[i-1];
+                        break;
+                    }
+                    j--;
+                }
+            }
+        }
+        return dp[n-1]+1;
     }
 
 
+    public  static  boolean checkShuZuYouHua(int[] nums,int target){
+        int i=0,j=0,sum=0;
+        while(i<nums.length){
+            while(sum<target && j<nums.length){
+                sum+=nums[j];
+                if(sum<target){
+                    j++;
+                }else if(sum==target){
+                    return true;
+                }else{
+                    break;
+                }
+            }
+            sum -=nums[i];
+            i++;
+        }
+
+        return false;
+    }
+
+    public static  boolean checkZiShuZu(int[] nums,int target){
+
+        for(int i=0;i<nums.length;i++){
+            int sum=nums[i];
+            for(int j=i+1;j<nums.length;j++){
+                if(sum==target){
+                    return true;
+                }else if(sum>target){
+                    break;
+                }
+                sum+=nums[j];
+            }
+        }
+        return false;
+    }
+
+
+    public static  boolean findDuiZi(int[] nums){
+        Arrays.sort(nums);
+        if(nums.length!=5){
+            return false;
+        }
+        if(nums[0]==1){
+            if(nums[4]==5){
+                for(int i=1;i<5;i++){
+                    if(nums[i]!=nums[i-1]+1){
+                        return false;
+                    }
+                }
+            }else if(nums[4]==13){
+                for(int i=2;i<5;i++) {
+                    if (nums[i] != nums[i - 1]+1) {
+                        return false;
+                    }
+                }
+            }else {
+                return false;
+            }
+        }else {
+            for(int i=1;i<5;i++){
+                if(nums[i]!=nums[i-1]+1){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+
+    public static int calculateMax(int[] nums){
+        int factor1 =0;
+        int factor2 = 0;
+        int res =0;
+        int sum =0;
+        for(int i=1;i<nums.length;i++) {
+            sum += nums[i];
+        }
+        if(sum<=1){
+            return 0;
+        }
+        int numZero = nums[0];
+        // 为true 则给factor1加上
+//        boolean firstFlag = true;
+
+        for(int i=nums.length-1;i>=1;i--){
+            while(nums[i]!=0){
+                if(factor1==0 || factor2==0){
+                    if(factor1==0){
+                        factor1  = factor1*10+i;
+                    }else{
+                        factor2  = factor2*10+i;
+                    }
+                }else{
+                    if(factor1<=factor2){
+                        factor1  = factor1*10+i;
+                    }else{
+                        factor2  = factor2*10+i;
+                    }
+                }
+                nums[i]--;
+            }
+        }
+        System.out.println(factor1);
+        System.out.println(factor2);
+        return (factor1*factor2*(int)Math.pow(10.0,numZero));
+    }
+
+    public static boolean findbaohan(int[][] nums){
+        int n =nums.length;
+        Arrays.sort(nums, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] t1, int[] t2) {
+                return t1[0]-t2[0];
+            }
+        });
+        for(int i =0;i<n;i++){
+            int j =i+1;
+            while(j<n && nums[j][0]<=nums[i][1]){
+                if(nums[j][1]<=nums[i][1]){
+                    return true;
+                }
+                j++;
+            }
+        }
+        return false;
+
+    }
 
 
 
